6a5b4febe685a6be49e2903eba8176013f64490f
skoontz1911 <67130504+skoontz1911@users.noreply.github.com>
2020-09-23 21:00:02 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,15 @@
-// Select color input
-// Select size input
 
+
+}
+var height, width, color;
 // When size is submitted by the user, call makeGrid()
-
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+	  height = $('#inputHeight').val();
+	  width = $('#inputWidth').val();
+	  makeGrid(height, width);
+	  console.log('Height: ' + height + 'and width: ' + width);
+}   
 function makeGrid() {
 
 // Your code goes here!
