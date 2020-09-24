947cb9173644ff4357c7f04969377b5db060f2bb
skoontz1911 <67130504+skoontz1911@users.noreply.github.com>
2020-09-24 19:54:06 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,17 +1,37 @@
 
 
-}
 var height, width, color;
+
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function (event) {
     event.preventDefault();
-	  height = $('#inputHeight').val();
-	  width = $('#inputWidth').val();
-	  makeGrid(height, width);
-	  console.log('Height: ' + height + ' and width: ' + width);
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+    //console.log('Height: ' + height + ' and width: ' + width);
 })   
-function makeGrid() {
+
+
+function makeGrid(x, y) {
+    $('tr').remove();
 
 // Your code goes here!
+    for (var i = 1; i <= x; i++) {
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+        for (var j = 1; j <=y; j++) {
+            $('#table' + i).append('<td></td>');
+        }
+    }
 
-}
+	
+    $('td').click(function addColor() {
+	color = $('#colorPicker').val();
+	    
+	if ($(this).attr('style')) {
+	    $(this).removeAttr('style')
+        } else {
+	    $(this).attr('style', 'background-color:' + color);
+        }
+    })
+}	
+	    
